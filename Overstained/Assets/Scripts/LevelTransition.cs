84e087fdf5d7a95c2f889605e148140407b6e7d7
Matthew Bengston <matt.bengston@epicgames.com>
2019-01-27 11:25:35 +0200

    Fixed player gui issue when dropping items

--- Overstained/Assets/Scripts/LevelTransition.cs
+++ Overstained/Assets/Scripts/LevelTransition.cs
@@ -7,33 +7,9 @@ public class LevelTransition : MonoBehaviour
 {
     public float transitionTime = 5f;
 
-    void Start()
-    {
-        UI.instance.transitionTimer.gameObject.SetActive(true);
-    }
-
-    void Update()
-    {
-        /*
-        transitionTime -= Time.deltaTime;
-
-        UI.instance.transitionTimer.text = "Next level in: " + transitionTime.ToString("0");
-
-        if(transitionTime < 0)
-        {
-            TransitionToLevel();
-        }
-        */
-    }
-
     public void TransitionToLevel()
     {
-        // TODO CHANGE TO NEXT LEVEL MATT
-
-        // Example test :D this will just load the next scene under build settings
-        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
         SceneManager.LoadScene($"Home_{Random.Range(1, 2)}");
-        //UI.instance.transitionTimer.text = "WE SHOULD CHANGE LEVEL. MATT HAS NOT IMPLEMENTED THIS YET!";
     }
 
     public void TransitionToMenu()
