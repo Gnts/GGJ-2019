7667d81eda39db1c82b226064ea365d2b9bd2a84
Gintautas Skersys <gintela05@gmail.com>
2019-01-27 14:57:28 +0200

    sfx

---
+++ Overstained/Assets/Scripts/VolumeSetter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeSetter : MonoBehaviour
+{
+
+    public AudioMixer master;
+
+    public void SetSoundMaster(float soundLevel)
+    {
+        master.SetFloat("masterVol", soundLevel);
+    }
+
+    public void SetSoundSfx(float soundLevel)
+    {
+        master.SetFloat("sfxVol", soundLevel);
+    }
+
+    public void SetSoundMusic(float soundLevel)
+    {
+        master.SetFloat("musicVol", soundLevel);
+    }
+
+    public void SetSoundHits(float soundLevel)
+    {
+        master.SetFloat("hitsVol", soundLevel);
+    }
+}
