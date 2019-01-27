7cb29c8f888aa7a91d9e9cc55e1abd258683f84f
Gintautas Skersys <gintela05@gmail.com>
2019-01-27 13:11:31 +0200

    hit fx

---
+++ Overstained/Assets/Scripts/ColisionAudio.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(AudioSource))]
+public class ColisionAudio : MonoBehaviour
+{
+    AudioSource source;
+    float resetTime = 0.1f;
+    float timer = 0f;
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        source.playOnAwake = false;
+        source.Stop();
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+    }
+
+    void OnEnable()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (Time.timeSinceLevelLoad < 0.1f) return;
+        if (timer > resetTime)
+        {
+            source.Stop();
+            source.Play();
+            timer = 0f;
+        }
+    }
+}
